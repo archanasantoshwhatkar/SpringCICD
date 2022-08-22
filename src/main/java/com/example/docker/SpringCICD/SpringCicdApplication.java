cd8f112eff59cc4250f7b3f154cdcce57dfaa09c
Archana <archana.whatkar@gmail.com>
2022-08-22 15:25:55 +0800

    Changed java version to 1.8

--- src/main/java/com/example/docker/SpringCICD/SpringCicdApplication.java
+++ src/main/java/com/example/docker/SpringCICD/SpringCicdApplication.java
@@ -24,6 +24,6 @@ public class SpringCicdApplication {
 //	git init
 //	git add README.md
 //		git commit -m "first commit"
-//		git branch -M main
+//		git branch -M master
 //		git remote add origin https://github.com/archanasantoshwhatkar/SpringCICD.git
-//		git push -u origin main
+//		git push -u origin master
