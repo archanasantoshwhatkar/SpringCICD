3cd2187077647da3e6920200795fdd509bad96a4
Archana <archana.whatkar@gmail.com>
2022-08-22 14:47:08 +0800

    first CI/CD Spring

---
+++ src/main/java/com/example/docker/SpringCICD/SpringCicdApplication.java
@@ -0,0 +1,29 @@
+package com.example.docker.SpringCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCicdApplication {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+
+		return "Welcome to JAVA Spring CI/CD!!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdApplication.class, args);
+	}
+
+}
+
+//	git init
+//	git add README.md
+//		git commit -m "first commit"
+//		git branch -M main
+//		git remote add origin https://github.com/archanasantoshwhatkar/SpringCICD.git
+//		git push -u origin main
